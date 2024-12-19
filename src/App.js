2429c81a6b79b11c6903c42a226f0def59b3068f
Deontre Fleming <deontre.fleming@hotmail.com>
2024-12-19 16:58:44 -0600

    App return

--- src/App.js
+++ src/App.js
@@ -77,7 +77,62 @@ function App() {
   
   return (
     <div className="App">
-     
+      <h1 className='app-name'>
+        My Weather App
+      </h1>
+      <div className='search-bar'>
+        <input
+          type='text'
+          className='city-search'
+          placeholder='Enter City Name..'
+          name='query'
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+          onKeyDownCapture={search}
+        />
+      </div>
+     {weather.loading && (
+      <>
+        <br />
+        <br />
+        <Oval type='Oval' color='black' height={100} width={100} />
+      </>
+     )}
+     {weather.error && (
+      <>
+        <br />
+        <br />
+        <span className='error-message'>
+          <FontAwesomeIcon icon={faFrown} />
+          <span style={{ fontSize: '20px' }}>City not found</span>
+        </span>
+      </>
+     )}
+     {weather && weather.data && weather.data.main && (
+      <div>
+        <div className='city-name'>
+          <h2>
+            {weather.data.name}, <span>{weather.data.sys.country}</span>
+          </h2>
+        </div>
+        <div className='date'>
+          <span>{toDateFunction()}</span>
+        </div>
+        <div className='icon-temp'>
+          <img 
+          className=''
+          src={`https://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`}
+          alt={weather.data.weather[0].description}
+          />
+          {Math.round(weather.data.main.temp)}
+          <sup className='deg'>°C</sup>
+        </div>
+        <div className="des-wind">
+          <p>{weather.data.weather[0].description.toUpperCase()}</p>
+          <p>Wind Speed: {weather.data.wind.speed}m/s</p>
+        </div>
+      </div>
+     )}
     </div>
   );
 }
